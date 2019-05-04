aee52bea3a7cda935998cc0d80e502c06fdbaa57
Karl Brand <brandk@gmail.com>
2019-05-05 00:35:18 +0200

    updated practical 5 and answers for May 2019

--- 05_basic_course_on_R_manipulating_data_practical/05_basic_course_on_R_manipulating_data_practical.R
+++ 05_basic_course_on_R_manipulating_data_practical/05_basic_course_on_R_manipulating_data_practical.R
@@ -1,46 +1,38 @@
-## 1.2 Use R to answer the following.
-## 1.2.1 Create a vector (using {c()}) called {a} (i.e. assign it to an 
-##       object called {a}) with four elements which are the integers 
-##       5 to 8 (inclusive).
+## ----quiet=T,eval=F------------------------------------------------------
+## length(letters)
 
-## 1.2.2 Display element 2 of {a}.
+## ----quiet=T,eval=F------------------------------------------------------
+## length(letters == LETTERS)
 
-## 1.2.3 Display element 4 of {a}.
+## ----quiet=T,eval=F------------------------------------------------------
+## which(letters %in% c("a", "d"))
 
-## 1.2.4 Calculate the product of elements 2 and 4 of {a}.
+## ----quiet=T,eval=F------------------------------------------------------
+## which(c("a", 7, "d") %in% letters)
 
-## 1.2.5 Assign the integers 3 and 4 to object {b} and use {b} to select elements 3 and 4 of object {a}.
+## ----quiet=T,eval=F------------------------------------------------------
+## letters[LETTERS > "W"]
 
-## 1.2.6 Display every element of {a} except element 2. 
+## ----quiet=T,eval=F------------------------------------------------------
+## letters[!LETTERS > "C"]
 
-## 1.2.7 Display every element of {a} except elements 3 and 4. 
+## ----quiet=T,eval=F------------------------------------------------------
+## seq(from = 1, to = 20, by = 3)
 
-## 1.2.8 Display only those elements of {a} that are greater than or equal to 6.
+## ----quiet=T-------------------------------------------------------------
+x <- matrix(8:11, nrow = 6, ncol = 4)
+x
 
-## 1.2.9 Display only those elements of {a} that are not equal to 7.
+## ----quiet=T,eval=F------------------------------------------------------
+## x + 4
+## x + x
+## 2 * x
+## x / c(2, 3, 4, 5)
+## x[, 3] + 2 * x[, 2]
+## nrow(x)
+## x[x[, 3] > 10, ]
 
-## 1.2.10 Use the {list} function to create an object {ab} which is a list of the two objects {a} and {b}.
-
-## 1.2.11 Display {ab}.
-
-## 1.2.12 Change the names of the elements in {ab} to ``a'' and ``b''.
-
-## 1.2.13 Display {ab} again. What has changed?
-
-## 1.2.14 Create this matrix {m}:
+## ------------------------------------------------------------------------
 m <- matrix(1:9, nrow = 3, byrow = T)
 m
-## Why are the numbers 1, 2, and 3 in the first row and not the first column? 
 
-## 1.2.15 Display the element on the second row and second column of {m}.
-
-## 1.2.16 item Display only the 2nd row of {m}.
-
-## 1.2.17 Display only the 3rd column of {m}.
-
-## 1.2.18 Display only the 2nd and 3rd columns of {m}. Do so in two different ways.
-
-## If you want to save your work: save your R session and/or source code!}
-
-  
-  
